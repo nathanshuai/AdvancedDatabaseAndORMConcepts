e3ce81638328040d9b25b7cb8484dcf5c633d5c6
nathanshuai <nathan.shuai@gmail.com>
2023-07-31 21:50:14 -0500

    Lab1

---
+++ AdvancedDatabaseAndORMConcepts/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace AdvancedDatabaseAndORMConcepts.Models
+{
+    public class Customer
+    {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+        public virtual List<Address> Addresses { get; set; }
+
+
+        public Customer() { }
+        public Customer(Guid iD, string firstName, string lastName, string companyName, string phone, List<Address> addresses)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            CompanyName = companyName;
+            Phone = phone;
+            Addresses = addresses;
+        }
+    }
+}
