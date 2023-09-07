12fb0f2b4c691796b77ae95daf9a3f0442a2f628
nathanshuai <nathan.shuai@gmail.com>
2023-09-07 15:07:11 -0500

    update

---
+++ AdvancedDatabaseAndORMConcepts/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿namespace AdvancedDatabaseAndORMConcepts.Models
+{
+    public enum Section
+    {
+        First,
+        Second,
+        Third
+    }
+
+    public class Room
+    {
+        [Key] 
+        public int RoomNumber { get; set; }
+
+        [Range(1, 6, ErrorMessage = "Capacity must be between 1 and 6.")]
+        public int Capacity { get; set; }
+
+        public Section Section { get; set; }
+    }
+}
